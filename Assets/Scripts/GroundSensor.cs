3bd2fc145af550a0813c4b0f9d80a7f4dc225c4f
LaiaGarciaCTS <lgarcia@ctsformacion.com>
2025-12-17 10:52:57 +0100

    fisicas  + salto

---
+++ Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    public bool isGrounded;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+
+        
+    }
+
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,15 +9,21 @@ public class PlayerControl : MonoBehaviour
 
     private InputAction moveAction;
     private Vector2 moveDirection;
+    private InputAction jumpAction;
 
 
     public Rigidbody2D rBody2D;
+    private SpriteRenderer renderer;
 
     public float jumpForce = 10;
 
     void Awake()
     {
         rBody2D = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+ 
+        moveAction = InputSystem.actions["Move"];
+        jumpAction = InputSystem.actions["Jump"];
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,9 +32,6 @@ public class PlayerControl : MonoBehaviour
         
         transform.position = startPosition;
 
-        moveAction = InputSystem.actions["Move"];
-
-        rBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
@@ -38,11 +41,35 @@ public class PlayerControl : MonoBehaviour
         
         transform.position = new Vector3(transform.position.x + moveDirection.x * movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
 
+    //otras maneras de hacer que el personaje se mueva de lado a lado en eje X.
         //transform.Translate(new Vector3(moveDirection.x * movementSpeed * Time.deltaTime, 0, 0));
 
         //transform.position = new Vector3(transform.position.x + moveDirection.x * movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
 
         //transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + moveDirection.x, transform.position.y), movementSpeed * Time.deltaTime);
 
+
+    //Cómo hacer flip al moverse.
+        if(moveDirection.x > 0)
+        {
+            renderer.flipX = false;
+        }
+
+        else if(moveDirection.x < 0)
+        {
+            renderer.flipX = true;
+        }
+    
+    //cómo saltar pulsando un botón.
+        if(jumpAction.WasPressedThisFrame())
+        {
+            rBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+       
+    //cómo eliminar la fracción.
+       void FixedUpdate()
+       {
+            rBody2D.linearVelocity = new Vector2(moveDirection.x * movementSpeed, rBody2D.linearVelocity.y);
+       }
     }
 }
